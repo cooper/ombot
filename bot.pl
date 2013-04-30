d6482e1ee2a682f1975d98cb6b1f10462eb97c83
Matthew Barksdale <mattwb65@gmail.com>
2013-04-30 23:30:46 +0000

    Add helper function for sending to irc, write code to send IRC intro

--- bot.pl
+++ bot.pl
@@ -19,7 +19,8 @@ use IO::Socket::IP;
 use Net::Async::Omegle;
 use Config::JSON;
 
-my ($mainLoop, $ircSocket, $asyncStream, $configFile, $config);
+my ($mainLoop, $configFile, $config);
+my ($youSocket, $youStream, $omSocket, $omStream);
 
 # Config file? Default to bot.conf unless otherwise told
 $configFile = $ARGV[0] || 'bot.conf';
@@ -29,8 +30,55 @@ $config = Config::JSON->new($configFile);
 
 # Initialization subroutine
 sub bot_init {
-    # Create socket
+    # Create loop
+    $mainLoop = IO::Async::Loop->new;
+    # Create sockets
+    $youSocket = IO::Socket::IP->new(
+        PeerAddr  => $config->get('host'),
+        PeerPort  => $config->get('port'),
+        LocalAddr => $config->get('bind'),
+        Timeout   => 10
+    );
+    $omSocket = IO::Socket::IP->new(
+        PeerAddr  => $config->get('host'),
+        PeerPort  => $config->get('port'),
+        LocalAddr => $config->get('bind'),
+        Timeout   => 10
+    );
+    # Create streams
+
+    # Send intros
+    send_intro();
+    # Let's go
+    $mainLoop->run;
+}
+
+# Send data to IRC
+sub irc_send
+{
+    my ($to, $data) = @_;
+    if (lc $to eq 'you')
+    {
+        $to = $youStream;
+    }
+    else
+    {
+        $to = $omStream;
+    }
+    chomp $data;
+    $to->write($data."\r\n");
+}
+
+sub send_intro
+{
+    # Send IRC intros
+    my %nicks = ('you' => $config->get('you/nick'), 'om' => $config->get('ombot/nick'));
+    foreach (qw/om you/)
+    {
+        irc_send($_, "NICK $nicks{$_}");
+        irc_send($_, "USER omegle * * :Omegle IRC Bot");
+    }
 }
 
 # Let's go
-$mainLoop->run;
+bot_init();
