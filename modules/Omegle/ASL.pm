1bd392fd20d6d2f6353f20af8cde2af37796988d
Mitchell Cooper <githubspam@rlygd.net>
2017-01-14 13:43:37 -0500

    fix asl conf fetching

--- modules/Omegle/ASL.pm
+++ modules/Omegle/ASL.pm
@@ -32,12 +32,12 @@ sub init {
         callback    => \&cmd_asl
     ) or return;
 
-    return 1;   
+    return 1;
 }
 
-sub conf   { $::conf->get('asl', @_)              }
-sub min () { $mod->{age_min} // conf('min') // 16 }
-sub max () { $mod->{age_max} // conf('max') // 26 }
+sub conf   { $::conf->get([ 'module', 'asl' ], @_)  }
+sub asl_min () { $mod->{age_min} // conf('min') // 16 }
+sub asl_max () { $mod->{age_max} // conf('max') // 26 }
 
 # setasl command.
 sub cmd_setasl {
@@ -45,20 +45,20 @@ sub cmd_setasl {
     my $sess = $channel->{sess};
     my $ages = join '', @args;
     my ($min, $max);
-    
+
     # no args; display current values.
     if (!scalar @args) {
         ($min, $max) = (min, max);
         $channel->send_privmsg("Current ASL age range: $min to $max");
         return;
     }
-    
+
     # age range must be in this format.
     if ($ages =~ m/^(\d+)\D+(\d+)$/) {
         $min = $1;
         $max = $2;
     }
-    
+
     # format is invalid.
     else {
         $channel->send_privmsg('Invalid age range format. Must be n-n.');
@@ -76,25 +76,25 @@ sub cmd_setasl {
 sub cmd_asl {
     my ($event, $user, $channel, @args) = @_;
     my $sess = $channel->{sess};
-    
+
     # not connected.
     $main::bot->om_connected($channel) or return;
-    
+
     my $sex = $args[0]; # supplied sex.
-    
+
     # fetch possibly values.
     my @ages  = (min..max);
     my @sexes = ('m', 'f');
     my @locs  = @{ conf('locations') || ['ca', 'fl'] };
-    
+
     # choose random values.
     my $age   =  $ages[ rand @ages  ];
     my $loc   =  $locs[ rand @locs  ];
        $sex ||= $sexes[ rand @sexes ];
-    
+
     # send the message to the stranger.
     $::bot->om_say($channel, "$age $sex $loc");
-    
+
 }
 
 $mod
