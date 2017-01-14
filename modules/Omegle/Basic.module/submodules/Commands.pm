1cce1b7689bf171aa92be816799b41d31f3938b7
Mitchell Cooper <githubspam@rlygd.net>
2017-01-14 13:53:44 -0500

    do not allow wpm to be inf or nan

--- modules/Omegle/Basic.module/submodules/Commands.pm
+++ modules/Omegle/Basic.module/submodules/Commands.pm
@@ -64,7 +64,7 @@ sub init {
     foreach (keys %commands) {
         $mod->register_command(command => $_, %{$commands{$_}}) or return;
     }
-    
+
     # register 100 priority start.
     $mod->register_command(
         command     => 'start',
@@ -73,7 +73,7 @@ sub init {
         name        => 'omegle.command.100-start',
         description => 'checks if a session is already running'
     );
-    
+
     # register -100 priority start.
     $mod->register_command(
         command     => 'start',
@@ -83,7 +83,7 @@ sub init {
         description => 'starts an Omegle conversation'
     );
 
-    return 1;   
+    return 1;
 }
 
 ########################
@@ -95,24 +95,24 @@ sub init {
 sub cmd_start_100 {
     my ($event, $user, $channel, @args) = @_;
     my $sess = $channel->{sess};
-    
+
     # check if a session already is running in this channel.
     if ($sess && $sess->running) {
         $channel->send_privmsg('There is already a session in progress.');
         return $event->stop;
     }
-    
+
     # no session is running. continue to execute any additional handlers.
     $event->{sess} = $main::om->new;
     return 1;
-    
+
 }
 
 # create and start a new session.
 # this callback with -100 priority will be called after any extensions.
 sub cmd_start_n100 {
     my ($event, $user, $channel, @args) = @_;
-    
+
     # create a new session if an earlier callback hasn't already.
     my $sess = $event->{sess} || $main::om->new;
     $channel->{sess} = $sess;
@@ -125,28 +125,28 @@ sub cmd_start_n100 {
 sub cmd_stop {
     my ($event, $user, $channel, @args) = @_;
     my $sess = $channel->{sess};
-    
+
     # check if a session already is running in this channel.
     if (!$sess || !$sess->running) {
         $channel->send_privmsg('No session is currently running.');
         $event->stop;
         return;
     }
-    
+
     # disconnect from Omegle.
     $sess->disconnect;
     $channel->send_privmsg('You have disconnected.');
-    
+
 }
 
 # send a typing event.
 sub cmd_type {
     my ($event, $user, $channel, @args) = @_;
     my $sess = $channel->{sess};
-    
+
     # not connected.
     $main::bot->om_connected($channel) or return;
-    
+
     $sess->type or return;
     $channel->send_privmsg('You are typing...');
 }
@@ -155,29 +155,29 @@ sub cmd_type {
 sub cmd_captcha {
     my ($event, $user, $channel, @args) = @_;
     my $sess = $channel->{sess};
-    
+
     # not connected.
     $main::bot->om_running($channel) or return;
-    
+
     # server is not waiting for a captcha response.
     if (!$sess->waiting_for_captcha) {
         $channel->send_privmsg('No captcha requires submission.');
         return;
     }
-    
+
     $channel->send_privmsg('Verifying...');
     $sess->submit_captcha(join ' ', @args);
-    
+
 }
 
 # send a message.
 sub cmd_say {
     my ($event, $user, $channel, @args) = @_;
     my $sess = $channel->{sess};
-    
+
     # not connected.
     $main::bot->om_running($channel) or return;
-    
+
     # determine the typing delay.
     # TODO: use the actual message substr'd.
     # connected check in om_say()
@@ -186,23 +186,23 @@ sub cmd_say {
     my $delay   = API::Module::Omegle::get_wpm_delay($message);
     $$delay_all += $delay;
     cmd_type($event, $user, $channel) if $delay;
-    
+
     # send the message after typing delay.
     my $timer = IO::Async::Timer::Countdown->new(
         delay     => $$delay_all,
         on_expire => sub {
             my $connected = $sess->connected;
-            
+
             # upcoming messages -- type again.
             cmd_type($event, $user, $channel)
                 if $delay && $$delay_all && $connected;
-                
+
             $$delay_all -= $delay;
             $connected or return;
             $main::bot->om_say($channel, $message);
         }
     );
-    
+
     $::loop->add($timer);
     $timer->start;
 }
@@ -237,18 +237,18 @@ sub cmd_status {
 sub cmd_setwpm {
     my ($event, $user, $channel, $set_wpm) = @_;
     my $wpm = \$API::Module::Omegle::wpm;
-    
+
     # setting.
     if (defined $set_wpm) {
-        if (!looks_like_number($set_wpm)) {
-            $channel->send_privmsg('WPM must be numeric.');
+        if ($set_wpm =~ m/\D/) {
+            $channel->send_privmsg('WPM must be an integer.');
             return;
         }
         $$wpm = $set_wpm;
         return $channel->send_privmsg('Fake typing disabled.') if !$set_wpm;
         return $channel->send_privmsg("Typing speed: $set_wpm wpm");
     }
-    
+
     my $real_wpm = API::Module::Omegle::wpm();
     $channel->send_privmsg(
         $real_wpm                       ?
